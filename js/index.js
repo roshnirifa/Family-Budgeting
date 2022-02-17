fb4062921c36170114c0eaaadca02862a944f582
roshnirifa <roshnirifa998@gmail.com>
2022-02-17 14:44:37 +0600

    balance added

--- js/index.js
+++ js/index.js
@@ -1,6 +1,4 @@
 
-
-
 document.getElementById('calculate-btn').addEventListener('click', function () {
     const foodInput = document.getElementById('food-input');
     const foodInputText = parseFloat(foodInput.value);
@@ -10,25 +8,50 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
     const rentInput = document.getElementById('rent-input');
     const rentInputText = parseFloat(rentInput.value);
 
-
     // clothes
     const clothesInput = document.getElementById('clothes-input');
     const clothesInputText = parseFloat(clothesInput.value);
-
-
     // calculate
-
     const calculate = foodInputText + rentInputText + clothesInputText;
     // total expenses
     const expenses = document.getElementById('total-expenses');
     const expensesText = expenses.innerText;
     expenses.innerText = calculate;
-
-    // balance
+    //balance
     const income = document.getElementById('income-input');
-    const incomeText = parseFloat(income.value);
+    let incomeText = parseFloat(income.value);
     const balance = document.getElementById('balance');
-    const balanceText = parseFloat(balance.innerText);
+    let balanceText = parseFloat(balance.innerText);
     balance.innerText = incomeText - expenses.innerText
+    if (typeof foodInputText != 'number' || typeof rentInputText != 'number' || typeof clothesInputText != 'number') {
+        return 'enter a number';
+    }
+    if (foodInputText < 0 || rentInputText < 0 || typeof clothesInputText < 0) {
+        return 'enter a positive number'
+    }
+    //  savings
+    document.getElementById('save-btn').addEventListener('click', function () {
+        const savingInput = document.getElementById('savings-input ');
+        const savingInputText = parseFloat(savingInput.value);
+        // saving amount
+
+        const savingAmount = document.getElementById('saving-amount');
+        const savingAmountText = parseFloat(savingAmount.innerText);
+        let totalSavingAmount = (income.value * savingInputText) / 100;
+        savingAmount.innerText = balance.innerText - (balance.innerText - totalSavingAmount);
+        // remaning balance 
+        const remaningInput = document.getElementById('remaining-input');
+        const remaningInputText = remaningInput.innerText;
+        remaningInput.innerText = balance.innerText - savingAmount.innerText;
+
+
+
+    })
+
+
+
 
 })
+
+
+
