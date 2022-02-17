fdefd97a0a4da9a00b85d4e615c470d30a643875
roshnirifa <roshnirifa998@gmail.com>
2022-02-17 18:29:58 +0600

    error handeling added

--- js/index.js
+++ js/index.js
@@ -22,23 +22,44 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
     let incomeText = parseFloat(income.value);
     const balance = document.getElementById('balance');
     let balanceText = parseFloat(balance.innerText);
+    // error handaling
+    if (isNaN(foodInputText) || isNaN(rentInputText) || isNaN(foodInputText) || isNaN(clothesInputText)) {
+        return alert('please input a number')
+    }
+    else if (foodInputText < 0 || rentInputText < 0 || typeof clothesInputText < 0) {
+        return alert('enter a positive number');
+    }
+
+
     balance.innerText = incomeText - expenses.innerText
-    if (typeof foodInputText != 'number' || typeof rentInputText != 'number' || typeof clothesInputText != 'number') {
-        return 'enter a number';
+    // error handeling for (expenses besi)
+    if (expenses.innerText > incomeText) {
+        return alert('your expenses is amount is more than your total income')
     }
-    if (foodInputText < 0 || rentInputText < 0 || typeof clothesInputText < 0) {
-        return 'enter a positive number'
-    }
+
     //  savings
     document.getElementById('save-btn').addEventListener('click', function () {
         const savingInput = document.getElementById('savings-input ');
         const savingInputText = parseFloat(savingInput.value);
+        // error handling
+        if (isNaN(savingInputText) || savingInputText < 0) {
+            return alert('please input a number')
+        }
+
         // saving amount
 
         const savingAmount = document.getElementById('saving-amount');
         const savingAmountText = parseFloat(savingAmount.innerText);
+
+
+
+
         let totalSavingAmount = (income.value * savingInputText) / 100;
         savingAmount.innerText = balance.innerText - (balance.innerText - totalSavingAmount);
+        // error for saving amount
+        if (savingAmount.innerText > balance.innerText) {
+            return alert('your saving amount is more than balance amount')
+        }
         // remaning balance 
         const remaningInput = document.getElementById('remaining-input');
         const remaningInputText = remaningInput.innerText;
